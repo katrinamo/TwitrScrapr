55622a34aaf749c4d6d77f2e93d9ea7a7d787b32
katrinamo <katieclong@uky.edu>
2018-04-23 20:13:12 -0400

    Gender categorizer now uses cmd line input
    This allows for an individual breakdown of inputs.

--- gender.py
+++ gender.py
@@ -1,18 +1,18 @@
 import requests
 import json
+import time
 
 def main():
-    f = open("MeTooTweets.txt", "r")
-    violenceTerms = ["sexual assault", "rape", "sexual harassment"]
-    vicVsSurv = ["victim", "survivor"]
+    term = raw_input("Which file would you like to use? ")
+    
+    f = open(term + " MeTooTweets.txt", "r")
 
     line = ""
     name = []
     prevLine =""
     fileLines = []  
   
-    outTerm = open("genderBreakdown.txt", "w")
-    outVicSurv = open("vicVsSurvGender.txt", "w")
+    outTerm = open(term+"GenderBreakdown.txt", "w")
 
     for line in f:
         fileLines.append(line)
@@ -20,83 +20,36 @@ def main():
     #determines which term was used by a tweeter
     used = -9
 
-    #Now we see which sexual violence descriptor is used
-    for i in range (2, len(fileLines)):
+   #Now we see which sexual violence descriptor is used
+    for i in range (8957, len(fileLines)):
         if fileLines[i-2]  == "\n" and fileLines[i-1] == "\n":
-            if violenceTerms[0] in fileLines[i+1]: 
-                used = 0
-            elif violenceTerms[1] in fileLines[i+1]:
-                used = 1
-            elif violenceTerms[2] in fileLines[i+1]:
-                used = 2
-            else:
-                used = -9
-            
-            if used != -9 :
-                name = fileLines[i].split()
-                r = requests.get("https://api.genderize.io/?name="+name[0]+"&country_id=us", verify = False)
-                results = r.json()
-                outTerm.write(violenceTerms[used]+" ")
-                if ('gender' in results):    
-                    if results['gender'] == None:
-                        outTerm.write(str(results['gender']) + " " )
-                        outTerm.write(str(results) + " ")
-                        outTerm.write(str(name))
-                        outTerm.write(" CHECK NEEDED**")                    
+            print i
+            name = fileLines[i].split()
+            r = requests.get("https://api.genderize.io/?name="+name[0]+"&country_id=us", verify = False)
+            results = r.json()
+    
+            if ('gender' in results):    
+                if results['gender'] == None:
+                    outTerm.write(str(results['gender']) + " " )
+                    outTerm.write(str(results) + " ")
+                    outTerm.write(str(name))
+                    outTerm.write(" CHECK NEEDED**")                    
+    
+                elif results['probability'] > 0.5 and results['gender'] != None:
+                    outTerm.write(results['gender'])
+    
+                else:    
+                    outTerm.write(str(results['gender']) + " " + str(results['probability']))
+                    outTerm.write(" " + str(results) + " ")
+                    outTerm.write(str(name))
+                    outTerm.write(" CHECK NEEDED**")                    
+                        
 
-                    elif results['probability'] > 0.5 and results['gender'] != None:
-                        outTerm.write(results['gender'])
-
-                    else:    
-                        outTerm.write(str(results['gender']) + " " + str(results['probability']))
-                        outTerm.write(" " + str(results) + " ")
-                        outTerm.write(str(name))
-                        outTerm.write(" CHECK NEEDED**")                    
-                    
-
-                    outTerm.write("\n")
+            outTerm.write("\n")
           
     outTerm.close()
-
-    #now we see which is used: victim vs survivor.
-    for i in range (2, len(fileLines)):
-        if fileLines[i-2]  == "\n" and fileLines[i-1] == "\n":
-            if vicVsSurv[0] in fileLines[i+1]: 
-                used = 0
-            elif vicVsSurv[1] in fileLines[i+1]:
-                used = 1
-            else:
-                used = -9
-            
-            if used != -9:    
-                name = fileLines[i].split()
-                r = requests.get("https://api.genderize.io/?name="+name[0]+"&country_id=us", verify = False)
-                results = r.json()
-                outVicSurv.write(vicVsSurv[used]+" ")
-    
-                if ('gender' in results): 
-                    if results['gender'] == None:
-                        outVicSurv.write(str(results['gender']) + " " )
-                        outVicSurv.write(str(results) + " ")
-                        outVicSurv.write(str(name))
-                        outVicSurv.write(" CHECK NEEDED**")                    
-    
-                    elif results['probability'] > 0.5 and results['gender'] != None:
-                        outVicSurv.write(results['gender'])
-    
-                    else:    
-                        outVicSurv.write(str(results['gender']) + " " + str(results['probability']))
-                        outVicSurv.write(" " + str(results) + " ")
-                        outVicSurv.write(str(name))
-                        outVicSurv.write(" CHECK NEEDED**")                    
-                    
-                    outVicSurv.write("\n")
-    
-    outVicSurv.close()
-
     f.close()
         
         
-
 
 main()
